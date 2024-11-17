66c9f522835bbf8b4221ddd5addc933f61607aed
Suheer Khan <khansuheer1@gmail.com>
2024-11-17 13:45:19 +0500

    Dependency Injected

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,19 +1,19 @@
 import 'package:learning_stacked/home/home_view.dart';
 import 'package:learning_stacked/profile/profile_view.dart';
+import 'package:learning_stacked/services/counter_service.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 @StackedApp(
   routes: [
-    MaterialRoute(
-      page: HomeView,
-    ),
+    MaterialRoute(page: HomeView, initial: true),
     MaterialRoute(
       page: ProfileView,
     ),
   ],
   dependencies: [
-    Singleton(classType: NavigationService)
+    Singleton(classType: NavigationService),
+    LazySingleton(classType: CounterService)
   ],
 )
 class App {}
